5d71ca7f35b2ade5f23192518acea4dae53ca5e7
M2C__ <m2c555555@gmail.com>
2025-05-08 23:29:08 +0700

    new home page

--- pksjWeb/pksjWeb/urls.py
+++ pksjWeb/pksjWeb/urls.py
@@ -43,3 +43,6 @@ handler404 = smart_404_redirect
 
 if settings.DEBUG is False:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
